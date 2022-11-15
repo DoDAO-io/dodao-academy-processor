c2bab261aa491dc43aea7d6cd14fdae007d98e7a
robin <robinnagpal.tiet@gmail.com>
2022-11-15 12:18:30 -0500

    fix test

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -2,6 +2,6 @@ import { validateGuide } from './../src/guides/validation/validateGuide';
 
 describe('validate course', () => {
   it('should pass for the course file', () => {
-    validateGuide(__dirname + '/dummy-academy/src/guides/course-contributor.yaml');
+    validateGuide(__dirname + '/dummy-academy/src/guides/main/course-contributor.yaml');
   });
 });
